e59a19bbdd68b7477970ef4609715ae8c652938c
SelenaVitaheit <164683743+SelenaVitaheit@users.noreply.github.com>
2026-02-19 01:05:58 +0300

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,10 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+#Bundler — это менеджер зависимостей для Ruby. Он:
+#•	Читает Gemfile
+#•	Устанавливает нужные гемы
+#•	Следит за версиями
+#•	Подключает их перед запуском приложения
+
